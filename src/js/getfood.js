177a50095b84853b44ed33dccd220cfa3f3dd9bd
Nieem <nieemcse019@gmail.com>
2024-09-17 19:54:51 +0600

    fix menu in mobile view

--- src/js/getfood.js
+++ src/js/getfood.js
@@ -1,6 +1,6 @@
 const getFood = (id="potato") => {
    // let inputText = document.getElementById("potato").value;
-   var mainpart = document.getElementById("mainpart");
+   
     //     var btns = header.getElementsByClassName("btn");
     //     for (var i = 0; i < btns.length; i++) {
     //     btns[i].addEventListener("click", function() {
@@ -14,11 +14,39 @@ const getFood = (id="potato") => {
 
       // mainpart.classList.add("-translate-y-6");
 
+      
+      
+      
+      // Create a MediaQueryList object
+      //var mainpart = document.getElementById("mainpart");
+     
     let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${id}`
     fetch(url)
         .then(res => res.json())
         .then(data => getitems(data.meals))
         .catch(error => console.log('Error: ', error));
+
+        var x = window.matchMedia("(max-width: 640px)")
+      
+        // start nav bar hide on scroll
+       var prevScrollpos = window.scrollY;
+           window.onscroll = function() {
+           var currentScrollPos = window.scrollY;
+           if (prevScrollpos > currentScrollPos) {
+               document.getElementById("nabbarhide").style.top = "0";
+           } else if(x.matches) {
+               document.getElementById("nabbarhide").style.top = "-80px";
+           }
+           else{
+               document.getElementById("nabbarhide").style.top = "-70px";
+           }
+           prevScrollpos = currentScrollPos;
+
+        }
+        //end nav bar hide on scroll
+
+
+
 }
 
 getFood();
@@ -35,13 +63,13 @@ const getitems = (meals) => {
     container.innerHTML = '';
     meals.forEach(meal => {
         let mealCard = document.createElement('div');    
-        mealCard.classList = 'card card-compact bg-base-100 shadow-xl border-solid  border-2 border-x-gray-300 hover:border-x-indigo-600';
+        mealCard.classList = 'card card-compact bg-base-100 shadow-xl border-solid  border-2 border-x-gray-300 hover:border-x-red-500';
         
         mealCard.innerHTML = `
             <figure>
                 <img src=${meal.strMealThumb} alt="Image of ${meal.strMeal}" />
             </figure>
-            <div class="card-body">
+            <div class="card-body border-solid border-1  border-gray-400">
                 <h2 class="card-title">${meal.strMeal}</h2>
                 <p title="${meal.strInstructions}" >${meal.strInstructions.slice(0,200)}...</p>
             </div>   
